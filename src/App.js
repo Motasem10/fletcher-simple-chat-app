fd8d180a7a6a9f9d6785329783713800b0b1d654
Motasem10 <moatasem.husien@gmail.com>
2019-06-30 09:58:38 -0700

    init firebase

--- src/App.js
+++ src/App.js
@@ -82,10 +82,51 @@ export default class App extends Component {
 //   }
 
 async componentDidMount(){
-console.log(1);
-const user= await firebase.auth().createUserWithEmailAndPassword('new@new.com','sdsp,dsd')
-console.log({user});
-}
+
+
+if(!firebase.apps.length){
+      var config = {
+        apiKey: "AIzaSyAE4zm_Ra_6gajcMZ9Kl6T-GaZDlT2ZwGo",
+        authDomain: "control-717c0.firebaseapp.com",
+        databaseURL: "https://control-717c0.firebaseio.com",
+        projectId: "control-717c0",
+        storageBucket: "",
+        messagingSenderId: "335441848101"
+      };
+    
+     firebase.initializeApp(config);
+    // firebase.auth().signOut();
+    }
+    AsyncStorage.getItem('USER').then(user=>{
+    if(!user) {
+      this.setState({isAuth:false})
+      firebase.auth().signOut();
+    }
+  // this.setState({isAuth:true})
+  if(user) this.setState({isAuth:true});})
+    
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) { 
+         console.log(user);
+             this.setState({ isAuth: true });
+                  
+      } else {
+        this.setState({ isAuth: false });
+     AsyncStorage.removeItem('Friends');
+     AsyncStorage.removeItem('USER');     
+        setCurrentUser({});
+      }
+    });
+  }
+
+
+
+
+
+
+
+
+
   renderNavigator() {
     //loading
     if (this.state.isAuth === null) {
